ab887777ca49dc3b33dfcea0aa03eedac15e9370
Ignacio Maiz <ignacio.maiz@gmail.com>
2022-02-28 23:00:44 +0000

    creating database

---
+++ src/db/__init__.py
@@ -0,0 +1,38 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file: str):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+            
+    return conn
+            
+            
+            
+
+def create_table(conn: sqlite3.Connection, create_table_sql: str):
+    """Create a table from the create_table_sql statement
+
+    Parameters
+    ----------
+    conn : sqlite3.Connection
+        Connection object
+    create_table_sql : str
+        a CREATE TABLE statement
+    """
+    if not create_table_sql.upper().startswith("CREATE TABLE"):
+        raise Error("SQL does not start with `CREATE TABLE`.")
+    
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
---
+++ src/db/create_discover_db.py
@@ -0,0 +1,62 @@
+from src.db import create_connection, create_table
+
+
+def main():
+    """Create sqlite Discover Enhanced database."""
+    database = r".\database\discover-enhanced.db"
+
+    sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
+                                id integer PRIMARY KEY,
+                                uid text NOT NULL,
+                                name text NOT NULL,
+                                join_date text
+                            ); """
+
+    sql_create_playlists_table = """CREATE TABLE IF NOT EXISTS playlists (
+                                    id integer PRIMARY KEY,
+                                    uid text NOT NULL,
+                                    name text NOT NULL,
+                                    user integer NOT NULL,
+                                    scrape_date text NOT NULL,
+                                    blob blob NOT NULL,
+                                    FOREIGN KEY (user) REFERENCES users (id)
+                                );"""
+
+    sql_create_songs_table = """CREATE TABLE IF NOT EXISTS songs (
+                                id integer PRIMARY KEY,
+                                uid text NOT NULL,
+                                name text NOT NULL,
+                                album text NOT NULL,
+                                blob blob NOT NULL,
+                            );"""
+
+    sql_create_playlists_songs_table = """CREATE TABLE IF NOT EXISTS playlists_songs (
+                                          id integer PRIMARY KEY,
+                                          playlist_id integer NOT NULL,
+                                          song_id integer NOT NULL,
+                                          FOREIGN KEY (playlist_id) REFERENCES playlists (id),
+                                          FOREIGN KEY (song_id) REFERENCES songs (id)
+                                      );"""
+
+    # create a database connection
+    conn = create_connection(database)
+
+    # create tables
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_users_table)
+
+        # create tasks table
+        create_table(conn, sql_create_playlists_table)
+
+        # create projects table
+        create_table(conn, sql_create_songs_table)
+
+        # create tasks table
+        create_table(conn, sql_create_playlists_songs_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == "__main__":
+    create_connection(r".\database\discover-enhanced.db")
